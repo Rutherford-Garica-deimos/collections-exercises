b61dca336cbb4ebae688bac893bfeff2a73b8218
Robert Rutherford <robertl.rutherford2@gmail.com>
2019-12-06 10:01:25 -0600

    exercise 2

--- src/Grocerys/Categories.java
+++ src/Grocerys/Categories.java
@@ -1,15 +1,18 @@
 package Grocerys;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Categories {
     private String category;
-    private ArrayList<Items> catList;
+    private ArrayList<Items> catList = new ArrayList<>();
 
 
-    public Categories(Items item){
-        assert false;
-        this.catList.add(item);
+    public Categories(){
+//        assert false;
+//        this.catList.add(item);
     }
 
     public String getCategory() {
@@ -18,5 +21,21 @@ public class Categories {
 
     public ArrayList<Items> getCatList() {
         return catList;
+    }
+
+    public void addItem(Items item){
+        this.catList.add(item);
+    }
+
+    public void sortList(){
+        assert false;
+        catList.sort(Comparator.comparing(Items::getName));
+    }
+
+    public void printItems(){
+        for (int i = 0; i <catList.size(); i++){
+            System.out.printf("Item - %s --- Number: %s%n",catList.get(i).getName(),catList.get(i).getNumber());
+        }
+        System.out.println();
     }
 }
